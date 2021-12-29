e5ca18e6f07215837a298b579f0e364edea2bcad
Mateusz Paprocki <mattpap@gmail.com>
2021-12-29 15:31:51 +0100

    Reduce assumptions about parent/root view

--- bokehjs/src/lib/models/layouts/layout_dom.ts
+++ bokehjs/src/lib/models/layouts/layout_dom.ts
@@ -19,9 +19,6 @@ import {SerializableState} from "core/view"
 export abstract class LayoutDOMView extends DOMComponentView {
   override model: LayoutDOM
 
-  override root: LayoutDOMView
-  override readonly parent: DOMComponentView
-
   override el: HTMLElement
 
   protected _child_views: Map<LayoutDOM, LayoutDOMView>
