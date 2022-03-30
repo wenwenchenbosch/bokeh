a53b8c348f1aa22fdae8008d8eca1194d869071c
Wenwen Chen (CR/AME5) <wenwen.chen@de.bosch.com>
2022-03-30 18:40:56 +0200

    add file input exmple to crossfilter app

--- examples/app/crossfilter/main.py
+++ examples/app/crossfilter/main.py
@@ -15,6 +15,10 @@ from bokeh.models import Select
 from bokeh.palettes import Spectral5
 from bokeh.plotting import curdoc, figure
 from bokeh.sampledata.autompg import autompg_clean as df
+from bokeh.layouts import column
+import time
+from bokeh.models import CustomJS, Div, FileInput
+from bokeh.plotting import output_file, show
 
 df = df.copy()
 
@@ -89,7 +93,31 @@ size.on_change('value', update)
 color = Select(title='Color', value='None', options=['None'] + continuous)
 color.on_change('value', update)
 
-controls = column(x, y, color, size, width=200)
+# Set up widgets
+file_input = FileInput(accept=".csv,.json")
+para = Div(text="<h1>FileInput Values:</h1><p>filename:<p>b64 value:")
+def upload_fit_data(attr, old, new):
+    print("fit data upload succeeded")
+    #print(file_input.value)
+    print(type(new))
+    #f = open(decoded, "r")
+    #contents = f.read()
+    #print(contents)
+# Create CustomJS callback to display file_input attributes on change
+callback = CustomJS(args=dict(para=para, file_input=file_input), code="""
+    para.text = "<h1>FileInput Values:</h1><p>filename: " + file_input.filename  + "<p>b64 value: " + file_input.value
+""")
+
+# Attach callback to FileInput widget
+file_input.on_change('filename', upload_fit_data)
+file_input.on_change('value', upload_fit_data)
+#file_input.js_on_change('value', callback)
+
+output_file("file_input.html")
+
+show(column(file_input, para))
+
+controls = column(x, y, color, size, file_input, width=200)
 layout = row(controls, create_figure())
 
 curdoc().add_root(layout)
--- examples/plotting/file/file_input.py
+++ examples/plotting/file/file_input.py
@@ -1,18 +1,27 @@
 from bokeh.layouts import column
+import time
 from bokeh.models import CustomJS, Div, FileInput
 from bokeh.plotting import output_file, show
 
 # Set up widgets
 file_input = FileInput(accept=".csv,.json")
 para = Div(text="<h1>FileInput Values:</h1><p>filename:<p>b64 value:")
-
+def upload_fit_data(attr, old, new):
+    print("fit data upload succeeded")
+    #print(file_input.value)
+    print(type(new))
+    #f = open(decoded, "r")
+    #contents = f.read()
+    #print(contents)
 # Create CustomJS callback to display file_input attributes on change
 callback = CustomJS(args=dict(para=para, file_input=file_input), code="""
     para.text = "<h1>FileInput Values:</h1><p>filename: " + file_input.filename  + "<p>b64 value: " + file_input.value
 """)
 
 # Attach callback to FileInput widget
-file_input.js_on_change('filename', callback)
+file_input.on_change('filename', upload_fit_data)
+file_input.on_change('value', upload_fit_data)
+file_input.js_on_change('value', callback)
 
 output_file("file_input.html")
 
